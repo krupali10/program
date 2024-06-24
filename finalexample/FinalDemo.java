cad2e3b2bba9cd783296ac83fa4f33dcd20766e5
Krupali kachhadiya <krupalikachhadiya28@gmail.com>
2024-06-24 10:31:00 +0530

    Add files via upload

---
+++ finalexample/FinalDemo.java
@@ -0,0 +1,12 @@
+package finalExample;
+
+public class FinalDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FinalExample f1 = new FinalExample();
+		f1.display();
+
+	}
+
+}
---
+++ finalexample/FinalExample.java
@@ -0,0 +1,19 @@
+package finalExample;
+
+public class FinalExample {
+	
+    public int r=2;
+	private final double pi = 3.14;
+	
+	public void display()
+	{
+		double circle = pi*r*r;
+		System.out.println(circle);
+	}
+	
+	/*public final void sayHello()
+	{
+		System.out.println("We learn Final");
+	}*/
+
+}
