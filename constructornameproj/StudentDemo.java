de6a87f22f7c223e7d864e34b332c4a3cc056fe5
Krupali kachhadiya <krupalikachhadiya28@gmail.com>
2024-06-24 10:10:11 +0530

    Add files via upload

---
+++ constructornameproj/StudentDemo.java
@@ -0,0 +1,15 @@
+package ConstructorNameproj;
+
+public class StudentDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Student s1 = new Student();
+		System.out.println("Student name 1 :"+s1);
+		
+		Student s2 = new Student("Avani");
+		System.out.println("Student name 2 :"+s2);
+	}
+
+}
